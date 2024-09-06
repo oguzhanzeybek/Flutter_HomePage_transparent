ff9e430f5441a9588a3905e6a168498eab00e9ed
Oguzhan Zeybek <darkstroge@gmail.com>
2024-09-06 23:59:08 +0300

    final the projeckt

--- flutter_transparent_homepage/lib/pages/Home_Page.dart
+++ flutter_transparent_homepage/lib/pages/Home_Page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_transparent_homepage/utils/utils.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +13,31 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      drawer: Drawer(),
+      body: Stack(
+        children: [
+          VxAnimatedBox()
+              .size(context.screenWidth, context.screenHeight)
+              .withGradient(
+                LinearGradient(
+                    colors: [aiColors.primarycolor1, aiColors.primarycolor2],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight),
+              )
+              .make(),
+          AppBar(
+            title: "Homepage".text.xl4.bold.white.make().shimmer(
+                  primaryColor: Vx.white,
+                  secondaryColor: const Color.fromARGB(255, 66, 121, 215),
+                ),
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            centerTitle: true,
+          ).h(100).p16(),
+        ],
+        fit: StackFit.expand,
+      ),
+    );
   }
 }
--- flutter_transparent_homepage/lib/utils/utils.dart
+++ flutter_transparent_homepage/lib/utils/utils.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
-mixin AiColors {
+mixin aiColors {
   static Color primarycolor1 = const Color.fromARGB(255, 211, 34, 167);
   static Color primarycolor2 = const Color.fromARGB(255, 128, 33, 217);
 }
