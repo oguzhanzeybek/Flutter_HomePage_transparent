b729bafcb67acbb0e319a95d2517bd9c4130c6d4
Oguzhan Zeybek <darkstroge@gmail.com>
2024-09-06 23:39:48 +0300

    Create the Home page

--- flutter_transparent_homepage/lib/main.dart
+++ flutter_transparent_homepage/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_transparent_homepage/pages/Home_Page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: HomePage(),
     );
   }
 }
